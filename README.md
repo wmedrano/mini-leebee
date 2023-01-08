00ccb92abcd2926ae120707c873088d98577253b
wmedrano <will.s.medrano@gmail.com>
2023-01-07 20:47:17 -0800

    Make window.

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Mini LeeBee
 
 Small sequencer aimed for the Raspberry Pi.
+
+## Building
+
+```shell
+cargo build --release
+target/release/mini-leebee
+```
+
+## Testing
+
+```shell
+cargo test
+```
--- mini-leebee/src/main.rs
+++ mini-leebee/src/main.rs
@@ -1,6 +1,8 @@
 use clap::Parser;
+use eframe::egui;
 use log::*;
 
+pub mod app;
 pub mod args;
 
 fn main() {
@@ -9,4 +11,14 @@ fn main() {
         .filter_level(log::LevelFilter::Info)
         .init();
     info!("{:?}", args);
+    let options = eframe::NativeOptions {
+        // The target is a small screen with 320x240 resolution.
+        initial_window_size: Some(egui::vec2(320.0, 240.0)),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "Mini LeeBee",
+        options,
+        Box::new(|_cc| Box::new(app::App {})),
+    )
 }
