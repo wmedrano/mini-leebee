de761b7f956e331fd2e86e9fbb8c3fcc99404291
Will <wmedrano@users.noreply.github.com>
2023-01-07 20:53:47 -0800

    Create window. (#1)
    * Add tests for CI.
    * Make window.

---
+++ mini-leebee/src/app.rs
@@ -0,0 +1,9 @@
+use eframe::egui;
+
+pub struct App {}
+
+impl eframe::App for App {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |_ui| {});
+    }
+}
