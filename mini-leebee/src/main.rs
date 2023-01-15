6db16fe726edae549b3c79406cce2d69e370e73d
Will <wmedrano@users.noreply.github.com>
2023-01-14 21:29:04 -0800

    remove gui (#8)

--- mini-leebee/src/main.rs
+++ mini-leebee/src/main.rs
@@ -1,19 +1,12 @@
 use clap::Parser;
-use eframe::egui;
 use log::*;
 
-pub mod app;
 pub mod args;
 
 fn main() {
     let args = args::Arguments::parse();
     env_logger::builder().filter_level(args.log_level).init();
     info!("{:?}", args);
-    let options = eframe::NativeOptions {
-        // The target is a small screen with 320x240 resolution.
-        initial_window_size: Some(egui::vec2(320.0, 240.0)),
-        ..Default::default()
-    };
 
     let audio_engine = jack_adapter::JackAdapter::new().unwrap();
     if args.auto_connect {
@@ -21,10 +14,5 @@ fn main() {
     } else {
         warn!("--auto_connect is set to false. Will not automatically connect ports.");
     }
-
-    eframe::run_native(
-        "Mini LeeBee",
-        options,
-        Box::new(|_cc| Box::new(app::App::new(audio_engine))),
-    )
+    std::thread::park();
 }
